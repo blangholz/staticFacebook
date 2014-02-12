5c7f1cde2037b996dde46613f5009ec208a601fb
Ben Langholz <blangholz@fb.com>
2014-02-11 20:27:22 -0800

    Initial Commit

---
+++ staticFacebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  staticFacebook
+//
+//  Created by Ben Langholz on 2/11/14.
+//  Copyright (c) 2014 Ben Langholz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ staticFacebookTests/staticFacebookTests.m
@@ -0,0 +1,34 @@
+//
+//  staticFacebookTests.m
+//  staticFacebookTests
+//
+//  Created by Ben Langholz on 2/11/14.
+//  Copyright (c) 2014 Ben Langholz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface staticFacebookTests : XCTestCase
+
+@end
+
+@implementation staticFacebookTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
